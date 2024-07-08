c23f09180b3b02d35dfa08cf6551fdc17a2fbe26
sumit <sk7627577@gmail.com>
2024-07-08 21:27:59 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,30 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        cream: {
+          primary: "#E9D175",
+        },
+        blue: {
+          dark: "#110D3C",
+        },
+        orange: {
+          primary: "#F26633",
+        },
+        gray: {
+          primary: "#797979",
+          secondary: "#F5F5F5",
+        },
+      },
+      fontSize: {
+        "6xl": ["60px", "70px"],
+        "5xl": ["42px", "50px"],
+        "4xl": ["36px", "44px"],
+        "3xl": ["25px", "32px"],
+        xl: ["18px", "30px"],
+        lg: ["15px", "25px"],
+        sm: ["12px", "20px"],
+      },
     },
   },
   plugins: [],
